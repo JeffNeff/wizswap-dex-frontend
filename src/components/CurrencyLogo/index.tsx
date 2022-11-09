a7ab1fd4342a39cc4fcd53229d32f0d32e17534b
levi0x <levi0x0@proton.me>
2022-11-09 02:08:29 +0200

    displaying gif image tokens

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -37,10 +37,10 @@ export default function CurrencyLogo({
 
     if (currency instanceof Token) {
       if (currency instanceof WrappedTokenInfo) {
-        return [...uriLocations, `/images/coins/${currency?.symbol ?? 'token'}.png`, getTokenLogoURL(currency.address)]
+        return [...uriLocations, `/images/coins/${currency?.symbol ?? 'token'}.png`, `/images/coins/${currency?.symbol ?? 'token'}.gif`, getTokenLogoURL(currency.address)]
       }
 
-      return [`/images/coins/${currency?.symbol ?? 'token'}.png`, getTokenLogoURL(currency.address)]
+      return [`/images/coins/${currency?.symbol ?? 'token'}.png`, `/images/coins/${currency?.symbol ?? 'token'}.gif`, getTokenLogoURL(currency.address)]
     }
     return []
   }, [currency, uriLocations])
